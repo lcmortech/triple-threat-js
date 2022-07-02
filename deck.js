1d59a7b4e6e41e149d9ea206dcea6911d77b13cf
Lauren Morgan <rg326@live.com>
2022-07-01 20:13:32 -0400

    moved files from old folder, added cards, cleaned up deck object

---
+++ deck.js
@@ -0,0 +1,200 @@
+/*cardDeck = {
+	'A of Hearts': 1,
+	'A of Diamonds': 1,
+	'A of Clubs': 1,
+	'A of Spades': 1,
+	'2 of Hearts': 2,
+	'2 of Diamonds': 2,
+	'2 of Clubs': 2,
+	'2 of Spades': 2,
+	'3 of Hearts': 3,
+	'3 of Diamonds': 3,
+	'3 of Clubs': 3,
+	'3 of Spades': 3,
+	'4 of Hearts': 4,
+	'4 of Diamonds': 4,
+	'4 of Clubs': 4,
+	'4 of Spades': 4,
+	'5 of Hearts': 5,
+	'5 of Diamonds': 5,
+	'5 of Clubs': 5,
+	'5 of Spades': 5,
+	'6 of Hearts': 6,
+	'6 of Diamonds': 6,
+	'6 of Clubs': 6,
+	'6 of Spades': 6,
+	'7 of Hearts': 7,
+	'7 of Diamonds': 7,
+	'7 of Clubs': 7,
+	'7 of Spades': 7,
+	'8 of Hearts': 8,
+	'8 of Diamonds': 8,
+	'8 of Clubs': 8,
+	'8 of Spades': 8,
+	'9 of Hearts': 9,
+	'9 of Diamonds': 9,
+	'9 of Clubs': 9,
+	'9 of Spades': 9,
+	'10 of Hearts': 10,
+	'10 of Diamonds': 10,
+	'10 of Clubs': 10,
+	'10 of Spades': 10,
+	'Jack of Hearts': 3,
+	'Jack of Diamonds': 3,
+	'Jack of Clubs': 3,
+	'Jack of Spades': 3,
+	'Queen of Hearts': 3,
+	'Queen of Diamonds': 3,
+	'Queen of Clubs': 3,
+	'Queen of Spades': 3,
+	'King of Hearts': 3,
+	'King of Diamonds': 3,
+	'King of Clubs': 3,
+	'King of Spades': 3,
+	'Joker A': 10,
+	'Joker B': 10 
+}*/
+
+cardDeck = {
+    ace: {
+            hearts:{
+                name: 'A of Hearts',
+                value: 1,
+                img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+				//'U+1F0B1 🂱'
+			},
+			diamonds:{
+                name: 'A of Diamonds',
+                value: 1,
+                img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+				//'U+1F0B1 🂱'
+			},
+			clubs:{
+                name: 'A of Clubs',
+                value: 1,
+                img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+				//'U+1F0B1 🂱'
+			},
+			spades:{
+                name: 'A of Spades',
+                value: 1,
+                img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+				//'U+1F0B1 🂱'
+			}
+
+        },
+	two: {
+        hearts:{
+            name: 'Two of Hearts',
+            value: 2,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		diamonds:{
+            name: 'Two of Diamonds',
+            value: 2,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		clubs:{
+            name: 'Two of Clubs',
+            value: 2,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		spades:{
+            name: 'Two of Spades',
+            value: 2,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		}
+
+    },
+
+	three: {
+		hearts:{
+            name: 'Three of Hearts',
+            value: 3,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		diamonds:{
+            name: 'Three of Diamonds',
+            value: 3,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		clubs:{
+            name: 'Three of Clubs',
+            value: 3,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		spades:{
+            name: 'Three of Spades',
+            value: 3,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		}
+		
+	},
+
+	four: {
+        hearts: {
+            name: 'Four of Hearts',
+            value: 4,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		diamonds: {
+            name: 'Four of Diamonds',
+            value: 4,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		clubs: {
+            name: 'Four of Clubs',
+            value: 4,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		spades: {
+            name: 'Four of Spades',
+            value: 4,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		}
+    },
+
+    five: {
+
+        hearts: {
+            name: 'Five of Hearts',
+            value: 5,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		diamonds:{
+            name: 'Five of Diamonds',
+            value: 5,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		clubs:{
+            name: 'Five of Clubs',
+            value: 5,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		},
+		spades:{
+            name: 'Five of Spades',
+            value: 5,
+            img: 'http://clipart-library.com/new_gallery/939134_ace-of-hearts-png.png'
+			//'U+1F0B1 🂱'
+		}
+    }
+  
+}
+
+/*
+*/
---
+++ index.js
@@ -0,0 +1,8 @@
+//Originally refactored from Python
+
+//Globals
+
+let myDeck = {}
+let compDeck = {}
+let myTurn = random.sample(list(hand), 3)
+let compTurn = random.sample(list(hand), 3)
