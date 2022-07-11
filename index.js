f87e1b5298047458d1088b9cae5af89b03a8abdf
Lauren Morgan <rg326@live.com>
2022-07-10 21:00:44 -0400

    added variables to index.js

--- index.js
+++ index.js
@@ -10,3 +10,4 @@ let compDeck = {}
 let myTurn = random.sample(list(hand), 3)
 let compTurn = random.sample(list(hand), 3)
 let deckContainer = document.querySelector('#deckContainer')
+let scoreBox = document.querySelector('#scoreBox')
