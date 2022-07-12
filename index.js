5147aef7414e9874e022b4c6b3daab619d3fb599
Lauren Morgan <rg326@live.com>
2022-07-11 23:14:57 -0400

    added notes

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ let myTurn = random.sample(list(hand), 3)
 let compTurn = random.sample(list(hand), 3)
 let deckContainer = document.querySelector('#deckContainer')
 let scoreBox = document.querySelector('#scoreBox')
+//add loops
