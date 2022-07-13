1c6457f9ef9fc083ef9c6aea231a9d272d739227
Lauren <rg326@live.com>
2022-07-12 21:57:20 -0400

    added more variables, div container

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ let myTurn = random.sample(list(hand), 3)
 let compTurn = random.sample(list(hand), 3)
 let deckContainer = document.querySelector('#deckContainer')
 let scoreBox = document.querySelector('#scoreBox')
+let testConsole = document.querySelector('#testConsole')
+
+//Actions (Functions)
+let currHand = cardDeck.keys()
+testConsole.textContent = 'test here'
